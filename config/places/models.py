442ebd259de235353fe4ca57ee3b8fc671b29903
Koroliov862 <dimakorolev35939@gmail.com>
2026-04-07 10:19:41 +0500

    two-apps-sepparatly

--- config/places/models.py
+++ config/places/models.py
@@ -2,6 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
+#Переместить модели Religion и Denomination в одноименные приложения religions и denominations
+
+#religions/models.py
+# from django.db import models
+
+# class Religion(models.Model):
+#     name = models.CharField(max_length=50, unique=True)
+#     display_name = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.display_name
+
+
+#denominations/models.py
+# from django.db import models
+# from religions.models import Religion
+
+# class Denomination(models.Model):
+#     name = models.CharField(max_length=50, unique=True)
+#     display_name = models.CharField(max_length=100)
+#     religion = models.ForeignKey(Religion, on_delete=models.CASCADE, related_name='denominations')
+
+#     def __str__(self):
+#         return self.display_name
+    
 class Religion(models.Model):
     name = models.CharField(max_length=50, unique=True)
     display_name = models.CharField(max_length=100)
