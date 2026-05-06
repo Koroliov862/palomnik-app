fe66327fb0279e80a7312a71fc41aadec34dd559
Koroliov862 <dimakorolev35939@gmail.com>
2026-05-06 12:12:54 +0500

    again-filling-databse

--- config/photos/models.py
+++ config/photos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class PlacePhoto(models.Model): #?
+class PlacePhoto(models.Model):
     religious_place = models.ForeignKey('places.ReligiousPlace', on_delete=models.CASCADE, related_name='photos')
     image_url = models.URLField()
     description = models.CharField(max_length=200, blank=True)
