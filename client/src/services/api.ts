53f576c989fb1d9a27c23331dfa14a63e110d6b4
Koroliov862 <dimakorolev35939@gmail.com>
2026-05-27 19:10:49 +0500

    first-victory

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Замените на реальный IP вашего компьютера в локальной сети
-const BASE_URL = 'http://192.168.88.97/api';
+const BASE_URL = 'http://192.168.0.97:8000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
