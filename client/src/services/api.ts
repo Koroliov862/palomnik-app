05570e792404de16082a3cfc6ebf8fa9b951a670
Koroliov862 <dimakorolev35939@gmail.com>
2026-05-27 13:33:03 +0500

    0121

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Замените на реальный IP вашего компьютера в локальной сети
-const BASE_URL = 'http://192.168.100.16/api';
+const BASE_URL = 'http://192.168.88.97/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
