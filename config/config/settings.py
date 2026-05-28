8bf76cf5ebf72d60af43d400f31f48c7331c3a86
Koroliov862 <dimakorolev35939@gmail.com>
2026-05-28 12:02:19 +0500

    save-images

--- config/config/settings.py
+++ config/config/settings.py
@@ -90,6 +90,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DATABASES = {
     'default': {
--- config/config/urls.py
+++ config/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from places.views import ReligiousPlaceViewSet
 from rest_framework.routers import DefaultRouter
@@ -38,3 +40,6 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
